a8e9a895dc22ceebdee1608853c00f70657b140e
Emersom <emersomnunes717@gmail.com>
2024-05-07 22:24:40 -0300

    Add database, and create table users

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response } from "express";
 import { routes } from "./Routes";
+import { migrationsRun } from "./database/migrations";
 
 const app = express();
 
 app.get("/", (request: Request, response: Response) => {
   response.send("Olá, testando")
 });
+
+migrationsRun();
 
 app.use(routes);
 const PORT: number = 3000;
